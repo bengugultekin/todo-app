c1a6d871cb4b3954676a52fc91b98e2d602662ff
bengu.gultekin <bengu.gultekin@tedbul.com.tr>
2023-03-17 01:14:20 +0300

    added action events for footer buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ function Footer() {
     return (
         <footer className="info">
             <p>Click to edit a todo</p>
-            <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
-            <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+            <p>Created by <a href="https://github.com/bengugultekin">Bengunur Gultekin</a></p>
+            <p>Part of <a href="https://github.com/bengugultekin/todo-app">Todo-App Oper Source Codes</a></p>
         </footer>
     );
 }
