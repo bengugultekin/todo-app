1178fca6531941e6f11616a29851113fc059406b
bengu.gultekin <bengu.gultekin@tedbul.com.tr>
2023-03-17 08:19:42 +0300

    update and delete records via API

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,23 +1,32 @@
 import {useState} from "react";
-import { useDispatch } from "react-redux";
-import { addTodo } from "../redux/todos/todosSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodoAsync } from "../redux/todos/todosSlice";
+import Loading from './Loading';
+import Error from './Error';
 
 function Form() {
     const [title, setTitle] = useState('');
     const dispatch = useDispatch();
-    const handleSubmit = (e) => {
+    const isLoading = useSelector((state) => state.todos.addNewTodoIsLoading);
+    const error = useSelector((state) => state.todos.addNewTodoError);
+    const handleSubmit = async (e) => {
         if (!title || title.replace(/ /g, '') === '') return;
         e.preventDefault();
-        dispatch(addTodo({title}));
+        await dispatch(addTodoAsync({title}));
         setTitle('');
-    }
+    };
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} style= {{ display: 'flex', alignItems: 'center' }}>
             <input className="new-todo" 
+            disabled={isLoading}
             placeholder="What needs to be done?" 
             autoFocus 
             value={title} 
             onChange={(e) => setTitle(e.target.value)}/>
+
+            {isLoading && <Loading />}
+            {error && <Error message={error}/>}
+            
         </form>
     );
 }
--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,5 +1,5 @@
 function Loading() {
-    return <div style={{ padding: 15, fontSize: 18 }}>Loading ...</div>;
+    return <div style={{ padding: 15, fontSize: 18 }}>Loading...</div>;
 }
 
 export default Loading;
